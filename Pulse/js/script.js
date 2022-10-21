ebb3fb7770c406e1220915e79422210a538370ae
IlluhaCatRage <111414430+IlluhaCatRage@users.noreply.github.com>
2022-10-21 16:38:28 +0600

    Update script.js

--- Pulse/js/script.js
+++ Pulse/js/script.js
@@ -62,7 +62,7 @@ $(document).ready(function(){
 
         $.ajax({
             type:"POST",
-            url:"Pulse/mailer/smart.php",
+            url:"mailer/smart.php",
             data: $(this).serialize()
         }).done(function(){
             $(this).find("input").val("");
