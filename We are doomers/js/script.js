0603838e207061a83529bc5357c35a26fbf46e0c
Damir Taganbaev <111414430+IlluhaCatRage@users.noreply.github.com>
2023-02-09 12:52:23 +0600

    Update script.js

--- We are doomers/js/script.js
+++ We are doomers/js/script.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
-    $('ul.tv__tabs').on('click', 'li:not(.tv__tab_active)', function() {
-        $(this)
-            .addClass('tv__tab_active').siblings().removeClass('tv__tab_active')
-            .closest('div.promo__tv').find('div.screen-item').removeClass('screen-item_active').eq($(this).index()).addClass('screen-item_active');
-    });
-
     $('.carousel__iner').slick({
-        // speed: 1200,
-        // //adaptiveHeight: true,
-        // prevArrow: '<button type="button" class="slick-prev"><img src="icon/carousel/left.png"></button>',
-        // nextArrow: '<button type="button" class="slick-next"><img src="icon/carousel/right.png"></button>',
         dots: false,
         arrows: false,
         autoplay: true,
         autoplaySpeed: 5000,
         fade: true,
         infinity: true
-      });
+     });
+    
+    $('ul.tv__tabs').on('click', 'li:not(.tv__tab_active)', function() {
+        $(this)
+            .addClass('tv__tab_active').siblings().removeClass('tv__tab_active')
+            .closest('div.promo__tv').find('div.screen-item').removeClass('screen-item_active').eq($(this).index()).addClass('screen-item_active');
+    });
 });
