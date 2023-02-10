164643cf90fc0bb0dd94bad90c589221483b5283
Damir Taganbaev <111414430+IlluhaCatRage@users.noreply.github.com>
2023-02-10 15:32:27 +0600

    Add files via upload

--- We are doomers/js/script.js
+++ We are doomers/js/script.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
-    $('.carousel__iner').slick({
-        // speed: 1200,
-        // //adaptiveHeight: true,
-        // prevArrow: '<button type="button" class="slick-prev"><img src="icon/carousel/left.png"></button>',
-        // nextArrow: '<button type="button" class="slick-next"><img src="icon/carousel/right.png"></button>',
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        fade: true,
-        infinity: true
-    });
+    (function init100vh(){
+        function setHeight() {
+          var vh = window.innerHeight * 0.01;
+          document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+        setHeight();
+        window.addEventListener('resize', setHeight);
+      })();
+    // $('.carousel__iner').slick({
+    //     // speed: 1200,
+    //     // //adaptiveHeight: true,
+    //     // prevArrow: '<button type="button" class="slick-prev"><img src="icon/carousel/left.png"></button>',
+    //     // nextArrow: '<button type="button" class="slick-next"><img src="icon/carousel/right.png"></button>',
+    //     dots: false,
+    //     arrows: false,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     fade: true,
+    //     infinity: true
+    // });
 
     $('ul.tv__tabs').on('click', 'li:not(.tv__tab_active)', function() {
         $(this)
